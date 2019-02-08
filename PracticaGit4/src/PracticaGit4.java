cee4fcbec0bdc928304be7a3c9fe822e17baf3df
zekan97 <ramirezticker97@gmail.com>
2019-02-08 10:49:08 +0100

    Mision cumplida

---
+++ PracticaGit4/src/PracticaGit4.java
@@ -0,0 +1,10 @@
+
+public class PracticaGit4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Practica compltada con exito");
+		System.out.println("----- +5 respect -----");
+	}
+
+}
